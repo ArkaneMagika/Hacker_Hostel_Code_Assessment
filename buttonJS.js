6085b262d147ee7f9c8b71bdcb7c4c098d1f32e8
Akane D <akane.simpson@gmail.com>
2017-06-08 16:25:18 -0500

    Quick Update Added some more lines in html file. Also added a bootstrap link to document. Still need to edit the JS file

---
+++ buttonJS.js
@@ -0,0 +1,16 @@
+// Making image and id variables
+var Dogbutton = "";
+var Catbutton = "";
+var Birdbuton = "";
+var imgSource = "";
+
+/*
+  if onClick.ID == "Dogbutton"
+    Do This
+  else if onClick.ID == "Birdbuton"
+    Do This
+  else is onClick.ID == "Catbutton"
+    Do This
+*/
+if document.getElementsByClassName('className')('id').onClick('btn-group btn-group-lg') == true
+  document.createElement()
