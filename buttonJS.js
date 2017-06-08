46a3486be7fd7121cd559946850a8d2997c9b81c
Akane D <akane.simpson@gmail.com>
2017-06-08 16:42:01 -0500

    Quick Update #3 Added if and 'for' loops for changing inner elements.

--- buttonJS.js
+++ buttonJS.js
@@ -1,16 +1,34 @@
 // Making image and id variables
-var Dogbutton = "";
-var Catbutton = "";
-var Birdbuton = "";
-var imgSource = "";
+var Dogbutton = ["","",""];
+var Catbutton = ["","",""];
+var Birdbuton = ["","",""];
 
-/*
+var displayImage = "";
+
+/*So this is how the loop should operate
   if onClick.ID == "Dogbutton"
     Do This
   else if onClick.ID == "Birdbuton"
     Do This
   else is onClick.ID == "Catbutton"
     Do This
+  When the initial condition is met a for loop starts 
 */
-if document.getElementsByClassName('className')('id').onClick('btn-group btn-group-lg') == true
-  document.createElement()
+if (true){
+  for (var i = 0; i < Dogbutton.length; i++) {
+    displayImage = Dogbutton[i]+"<br>";
+    document.innerHTML();
+  }
+}
+else if (true) {
+  for (var i = 0; i < Catbutton.length; i++) {
+    displayImage = Catbutton[i]+"<br>"
+    document.innerHTML();
+  }
+}
+else if (true) {
+  for (var i = 0; i < Birdbuton.length; i++) {
+    displayImage = Birdbuton[i]+"<br"
+    document.innerHTML();
+  }
+}
